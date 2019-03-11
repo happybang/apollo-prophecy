af1f2c0e9ca64f7f099ee749da81c1a452e3e7d8
fengwan <fengwan@staff.sina.com.cn>
2019-03-11 14:59:53 +0800

    1

--- lib/commands/ask/ask.d.ts
+++
@@ -1,7 +0,0 @@
-export interface AskArgs {
-    input?: string;
-    outputFilePath?: string;
-    errorField?: string;
-    headers?: string[];
-}
-export default function ask({ input, errorField, headers, outputFilePath }: AskArgs): Promise<string>;
--- lib/writeClassFile.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (content: string, output?: string) => string;
-export default _default;
